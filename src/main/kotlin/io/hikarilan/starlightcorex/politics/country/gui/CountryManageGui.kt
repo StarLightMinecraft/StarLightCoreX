04c980859b998fe1b41f6e5fe6e4a4b8a951d730
HikariLan <shaokeyibb@126.com>
2022-06-28 13:53:18 +0800

    country current manage

--- src/main/kotlin/io/hikarilan/starlightcorex/politics/country/gui/CountryManageGui.kt
+++ src/main/kotlin/io/hikarilan/starlightcorex/politics/country/gui/CountryManageGui.kt
@@ -1,11 +1,20 @@
 package io.hikarilan.starlightcorex.politics.country.gui
 
+import io.hikarilan.starlightcorex.economy.account.AccountActionSource
+import io.hikarilan.starlightcorex.economy.account.AccountOperator
+import io.hikarilan.starlightcorex.economy.account.SystemAccountOperator
+import io.hikarilan.starlightcorex.economy.account.gui.AccountManageGui
+import io.hikarilan.starlightcorex.economy.currency.CountryLocalCurrency
+import io.hikarilan.starlightcorex.economy.currency.CountryRemoteCurrency
+import io.hikarilan.starlightcorex.economy.currency.Currency
+import io.hikarilan.starlightcorex.economy.currency.LocalCurrency
 import io.hikarilan.starlightcorex.gui.GuiBase
 import io.hikarilan.starlightcorex.gui.PageScrollableGui
 import io.hikarilan.starlightcorex.gui.exitButton
 import io.hikarilan.starlightcorex.person.Human
 import io.hikarilan.starlightcorex.politics.country.Country
 import io.hikarilan.starlightcorex.region.Region
+import io.hikarilan.starlightcorex.storage.getAllStorageFor
 import io.hikarilan.starlightcorex.storage.getStorageFor
 import io.hikarilan.starlightcorex.utils.*
 import io.hikarilan.starlightcorex.utils.ComponentUtils.toMiniMessage
@@ -16,6 +25,7 @@ import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.Material
 import org.bukkit.event.inventory.ClickType
 import org.bukkit.inventory.ItemStack
+import java.math.BigDecimal
 import java.time.Instant
 
 class CountryManageGui(
@@ -79,10 +89,150 @@ class CountryManageGui(
         initInventory()
     }
 
-    inner class CountryCurrencyGui: GuiBase(owner, proxied, title = Component.text("货币管理").color(primaryColor)) {
+    inner class CountryCurrencyGui : GuiBase(owner, proxied, title = Component.text("货币管理").color(primaryColor)) {
 
         override val builder: MutableMap<Int, GuiElement>.() -> Unit = {
-
+            if (country.currency == null) {
+                put(0, GuiElement(ItemStack(Material.GOLD_INGOT).apply {
+                    editMeta {
+                        it.displayName(Component.text("未发行货币").color(primaryColor))
+                        it.lore(buildList {
+                            add(
+                                Component.text("左键点击以发行新货币").color(secondaryColorVariant)
+                                    .decoration(TextDecoration.ITALIC, false)
+                            )
+                            add(
+                                Component.text("右键点击以使用他国已发行货币作为本国货币").color(secondaryColorVariant)
+                                    .decoration(TextDecoration.ITALIC, false)
+                            )
+                        })
+                    }
+                }, close = true) { event ->
+                    if (event.isLeftClick) {
+                        buildConversation(forWhom = proxied) {
+                            add(ConversationSection(prompt = {
+                                Component.text("请输入要创建的货币名称")
+                                    .color(secondaryColor)
+                            }, validate = { input, _ -> input?.isNotBlank() == true }, acceptInput = { input, _ ->
+                                mapOf("name" to input!!.trim())
+                            }))
+                            add(ConversationSection(prompt = {
+                                Component.text("请输入要创建的货币标识，如$")
+                                    .color(secondaryColor)
+                            }, validate = { input, _ -> input?.isNotBlank() == true }, acceptInput = { input, _ ->
+                                mapOf("symbol" to input!!.trim())
+                            }))
+                            add(messageConversationSection { context ->
+                                val currency =
+                                    CountryLocalCurrency(context["name"] as String, context["symbol"] as String)
+                                country.currency = currency
+                                Component.text("已创建货币：").append(Component.text(currency.name)).color(primaryColor)
+                            })
+                        }.begin()
+                    } else {
+                        buildConversation(forWhom = proxied) {
+                            add(ConversationSection(prompt = {
+                                Component.text("请输入要关联到的货币名称或标识符")
+                                    .color(secondaryColor)
+                            }, validate = { input, _ ->
+                                getAllStorageFor<Currency>().any { it.type == Currency.Type.LOCAL && (it.name == input || it.uniqueId.toString() == input) }
+                            }, acceptInput = { input, _ ->
+                                mapOf("currency" to getAllStorageFor<Currency>().find { it.name == input || it.uniqueId.toString() == input }!!)
+                            }))
+                            add(messageConversationSection { context ->
+                                val currency = (context["currency"] as LocalCurrency)
+                                country.currency = CountryRemoteCurrency(currency.uniqueId)
+                                Component.text("已关联到货币：").append(Component.text(currency.name)).color(primaryColor)
+                            })
+                        }.begin()
+                    }
+                })
+            } else {
+                put(0, GuiElement(ItemStack(Material.GOLD_INGOT).apply {
+                    editMeta {
+                        it.displayName(Component.text("货币总览").color(primaryColor))
+                        it.lore(buildList {
+                            add(
+                                Component.text("货币名称：").append(Component.text(country.currency!!.name))
+                                    .color(secondaryColorVariant)
+                                    .decoration(TextDecoration.ITALIC, false)
+                            )
+                            add(
+                                Component.text("货币标识：").append(Component.text(country.currency!!.symbol))
+                                    .color(secondaryColorVariant)
+                                    .decoration(TextDecoration.ITALIC, false)
+                            )
+                            add(
+                                Component.text("货币类型：").append(
+                                    when (country.currency!!.type) {
+                                        Currency.Type.LOCAL -> Component.text("本国货币")
+                                        Currency.Type.REMOTE -> Component.text("他国货币")
+                                    }
+                                ).color(secondaryColorVariant)
+                                    .decoration(TextDecoration.ITALIC, false)
+                            )
+                            add(
+                                Component.text("货币标识符：").append(Component.text(country.currency!!.uniqueId.toString()))
+                                    .color(secondaryColorVariant)
+                                    .decoration(TextDecoration.ITALIC, false)
+                            )
+                        })
+                    }
+                }))
+            }
+            put(1, GuiElement(ItemStack(Material.EMERALD).apply {
+                editMeta { it.displayName(Component.text("国库管理").color(primaryColor)) }
+            }) {
+                AccountManageGui(owner, proxied, country.account).openGUI()
+            })
+            if (country.currency is LocalCurrency) {
+                put(2, GuiElement(ItemStack(Material.PAPER).apply {
+                    editMeta {
+                        it.displayName(Component.text("发行货币").color(primaryColor))
+                        it.lore(buildList {
+                            add(
+                                Component.text("发行任意单位的货币").append(Component.text(country.currency!!.name))
+                                    .color(secondaryColorVariant)
+                                    .decoration(TextDecoration.ITALIC, false)
+                            )
+                            add(
+                                Component.text("新发行的货币将被添加到您的国库账户中").append(Component.text(country.currency!!.name))
+                                    .color(secondaryColorVariant)
+                                    .decoration(TextDecoration.ITALIC, false)
+                            )
+                            add(
+                                Component.text("然后，您便可通过国库转账系统分发您的货币").append(Component.text(country.currency!!.name))
+                                    .color(secondaryColorVariant)
+                                    .decoration(TextDecoration.ITALIC, false)
+                            )
+                        })
+                    }
+                }) {
+                    buildConversation(forWhom = proxied) {
+                        add(ConversationSection(prompt = {
+                            Component.text("请输入要发行的货币数量")
+                                .color(secondaryColor)
+                        }, validate = { input, _ ->
+                            input?.toBigDecimalOrNull() != null
+                        }, acceptInput = { input, _ ->
+                            mapOf("amount" to input!!.toBigDecimal())
+                        }))
+                        add(messageConversationSection { context ->
+                            val amount = (context["amount"] as BigDecimal)
+                            country.account.deposit(country.currency!!, amount, object : AccountActionSource {
+                                override val operator: AccountOperator = SystemAccountOperator
+                                override val name: String = "发行货币"
+                                override val reason: String = "发行本国货币"
+                            })
+                            Component.text("已发行货币：")
+                                .append(Component.text(country.currency!!.name))
+                                .append(Component.text(country.currency!!.format(amount)))
+                                .append(Component.text(country.currency!!.symbol))
+                                .color(primaryColor)
+                        })
+                    }.begin()
+                })
+            }
         }
 
         init {
@@ -129,7 +279,7 @@ class CountryManageGui(
                     country.enterMessage = null
                     proxied.sendMessage(Component.text("已清除入境提示语"))
                 } else {
-                    buildConversation(proxied){
+                    buildConversation(proxied) {
                         add(ConversationSection(
                             prompt = {
                                 Component.text("请输入入境提示语，支持 MiniMessage 语法").color(secondaryColor)
@@ -137,11 +287,11 @@ class CountryManageGui(
                             validate = { input, _ ->
                                 input?.isNotBlank() == true
                             },
-                            acceptInput = { input,_->
+                            acceptInput = { input, _ ->
                                 mapOf("message" to input!!.toMiniMessage())
                             }
                         ))
-                        add(messageConversationSection {context->
+                        add(messageConversationSection { context ->
                             country.enterMessage = context["message"] as Component
                             Component.text("已更改入境提示语为: ")
                                 .append(context["message"] as Component)
@@ -165,7 +315,7 @@ class CountryManageGui(
                     country.leaveMessage = null
                     proxied.sendMessage(Component.text("已清除出境提示语"))
                 } else {
-                    buildConversation(proxied){
+                    buildConversation(proxied) {
                         add(ConversationSection(
                             prompt = {
                                 Component.text("请输入出境提示语，支持 MiniMessage 语法").color(secondaryColor)
@@ -173,11 +323,11 @@ class CountryManageGui(
                             validate = { input, _ ->
                                 input?.isNotBlank() == true
                             },
-                            acceptInput = { input,_->
+                            acceptInput = { input, _ ->
                                 mapOf("message" to input!!.toMiniMessage())
                             }
                         ))
-                        add(messageConversationSection {context->
+                        add(messageConversationSection { context ->
                             country.leaveMessage = context["message"] as Component
                             Component.text("已更改出境提示语为: ")
                                 .append(context["message"] as Component)
@@ -195,7 +345,7 @@ class CountryManageGui(
                         add(Component.text("点击以切换设置").color(secondaryColor))
                     })
                 }
-            }){
+            }) {
                 country.isAllowJoin = !country.isAllowJoin
                 proxied.sendMessage(Component.text("状态已切换为: ${country.isAllowJoin}"))
                 reloadInventory()
